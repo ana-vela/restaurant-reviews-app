62aa2b99390e866eea54a0b6023010d7c859212f
ana-vela <ana@anavela.net>
2020-07-16 19:56:28 -0600

    Adding semicolon

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = `http://reviews-restaurants.netlify.app/data/restaurants.json` // Change this to your server port
+    const port = `http://reviews-restaurants.netlify.app/data/restaurants.json`; // Change this to your server port
     return `http://reviews-restaurants.netlify.app/data/restaurants.json`;
   }
 
